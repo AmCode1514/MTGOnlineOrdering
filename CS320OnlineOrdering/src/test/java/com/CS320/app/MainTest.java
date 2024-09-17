31e2b8014b255e99caecbdc83043899ab64371b5
Brent Jones <brentjones135@gmail.com>
2024-09-17 12:43:43 -0600

    Maven and git stuff

---
+++ CS320OnlineOrdering/src/test/java/com/CS320/app/MainTest.java
@@ -0,0 +1,19 @@
+package com.CS320.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
