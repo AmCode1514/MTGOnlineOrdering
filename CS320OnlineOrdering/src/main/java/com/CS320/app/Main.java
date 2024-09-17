0fcbafeba8cba3391f314fd9852af1e4b69ca3b9
Brent Jones <brentjones135@gmail.com>
2024-09-17 14:51:47 -0600

    Added Javalin, configured jar dependencies, and added gson

--- CS320OnlineOrdering/src/main/java/com/CS320/app/Main.java
+++ CS320OnlineOrdering/src/main/java/com/CS320/app/Main.java
@@ -3,9 +3,12 @@ import com.CS320.app.requests.*;
 /**
  * Hello world!
  */
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        request.print();
+        var app = Javalin.create(/*config*/)
+            .get("/", ctx -> ctx.result("Hello World"))
+            .start(7070);
     }
 }
