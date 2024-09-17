015e16002e6e253b843e837b747e30779cfc30d4
Brent Jones <brentjones135@gmail.com>
2024-09-17 13:07:23 -0600

    ignore

--- CS320OnlineOrdering/src/main/java/com/CS320/app/Main.java
+++ CS320OnlineOrdering/src/main/java/com/CS320/app/Main.java
@@ -1,10 +1,11 @@
 package com.CS320.app;
-
+import com.CS320.app.requests.*;
 /**
  * Hello world!
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        request.print();
     }
 }
---
+++ CS320OnlineOrdering/src/main/java/com/CS320/app/requests/request.java
@@ -0,0 +1,7 @@
+package com.CS320.app.requests;
+
+public class request {
+    public static void print() {
+        System.out.println(1);
+    }
+}
