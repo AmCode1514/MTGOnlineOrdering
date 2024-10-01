dd6eb49fe0c7e675e4156a6b6a426d512e259725
Brent Jones <brentjones135@gmail.com>
2024-10-01 13:57:09 -0600

    More stuff

--- CS320OnlineOrdering/src/main/java/com/CS320/app/Main.java
+++ CS320OnlineOrdering/src/main/java/com/CS320/app/Main.java
@@ -7,8 +7,9 @@ import io.javalin.Javalin;
 
 public class Main {
     public static void main(String[] args) {
-        var app = Javalin.create(/*config*/)
-            .get("/", ctx -> ctx.result("Hello World"))
-            .start(7070);
+
+        // var app = Javalin.create(/*config*/)
+        //     .get("/", ctx -> ctx.result("Hello World"))
+        //     .start(7070);
     }
 }
